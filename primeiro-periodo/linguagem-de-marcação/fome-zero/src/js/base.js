f67e88b5a2a32458ae125932a653f84ee461536d
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-03-30 20:18:42 -0300

    added files, organize and more

--- primeiro-periodo/linguagem-de-marcação/fome-zero/src/js/base.js
+++
@@ -1,6 +0,0 @@
-const $html = document.querySelector('html')
-const $checkbox = document.querySelector('#checkbox-theme')
-
-$checkbox.addEventListener('change', function() {
-	$html.classList.toggle('dark-mode')
-})
